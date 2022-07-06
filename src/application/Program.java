db2fcf5c1470f621b293e8557971432bbae76202
joaovitorsb97 <jvs.bastos@gmail.com>
2022-07-06 14:28:59 -0300

    DepartmentDao Complete Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -14,6 +14,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		List<Seller> list = new ArrayList<>();
+		List<Department> listDepartment = new ArrayList<>();
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -45,10 +46,38 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("Update Completed!");
 		
-		System.out.println("\n=== TEST 5: seller delete ===");
+		/* System.out.println("\n=== TEST 5: seller delete ===");
 		seller = sellerDao.findById(11);
 		sellerDao.deleteById(seller.getId());
-		System.out.println("Delete Completed!");
+		System.out.println("Delete Completed!"); */
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("\n=== TEST DEPARTMENT ===");
+		System.out.println("=== TEST 1: department findById ===");
+		Department department = departmentDao.findById(2);
+		System.out.println(department);
+		
+		System.out.println("\n=== TEST 2: department findById ===");
+		listDepartment = departmentDao.findAll();
+		for(Department obj : listDepartment) {
+			System.out.println(obj);
+		}
+		
+	/*	System.out.println("\n=== TEST 3: department insert ===");
+		departmentDao.insert(new Department(null, "Games"));
+		System.out.println("Department Inserted With Success!"); */
+		
+		System.out.println("\n=== TEST 4: department update ===");
+		department = departmentDao.findById(7);
+		department.setName("Cellphones");
+		departmentDao.update(department);
+		System.out.println("Update complete!");
+		
+		System.out.println("\n=== TEST 5: department delete ===");
+		department = departmentDao.findById(6);
+		departmentDao.deleteById(department.getId());
+		System.out.println("Delete complete!");
 	}
 
 }
